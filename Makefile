96075b120c1d999a1723e1fff1b4a76c80eb73d2
Marius Meier <mmeier@student.hive.fi>
2025-01-15 13:32:25 +0200

    adds parseChannelModes file

--- Makefile
+++ Makefile
@@ -24,7 +24,7 @@ FLAGS = -Wall -Wextra -Werror -std=c++11 -fsanitize=address
 SRC_DIR = ./src
 OBJ_DIR = obj
 
-FILES = main.cpp Server.cpp Client.cpp runServer.cpp messageHandler.cpp Channel.cpp handleCommands.cpp
+FILES = main.cpp Server.cpp Client.cpp runServer.cpp messageHandler.cpp Channel.cpp handleCommands.cpp #parseChannelModes.cpp
 
 OBJ_FILES = $(addprefix $(OBJ_DIR)/, $(FILES:.cpp=.o))
 
---
+++ src/parseChannelModes.cpp
@@ -0,0 +1,18 @@
+/* **************************************************************************************** */
+/*                                                                                          */
+/*                                                        ::::::::::: :::::::::   ::::::::  */
+/*                                                           :+:     :+:    :+: :+:    :+:  */
+/*                                                          +:+     +:+    +:+ +:+          */
+/*                                                         +#+     +#++:++#:  +#+           */
+/*  By: Timo Saari<tsaari@student.hive.fi>,               +#+     +#+    +#+ +#+            */
+/*      Matti Rinkinen<mrinkine@student.hive.fi>,        #+#     #+#    #+# #+#    #+#      */
+/*      Marius Meier<mmeier@student.hive.fi>        ########### ###    ###  ########        */
+/*                                                                                          */
+/* **************************************************************************************** */
+
+#include "Channel.hpp"
+#include "Server.hpp"
+
+void Server::handleMode(Client &client, std::vector<std::string> tokens, int index) {
+	
+}
